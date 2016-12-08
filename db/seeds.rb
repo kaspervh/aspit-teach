087e488af8f7bcab7d9c402a3b0ad8790b64d647
kaspervh <kaspervhauschildt@gmail.com>
2016-12-08 13:39:33 +0100

    made changes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+School.destroy_all
+Grade.destroy_all
+Role.destroy_all
+User.destroy_all
+
+school = School.create(name: "Aspit østjylland")
 
 
-School.create(name: "Aspit østjylland")
+grade = Grade.create(name: "Ikke Tilknyttet klasse")
 
+student = Role.create(name: "Student")
+teacher = Role.create(name: "Teacher")
+mentor = Role.create(name: "Mentor")
+admin = Role.create(name: "Admin")
+super_admin = Role.create(name: "Super admin")
 
-Grade.create(name: "Ikke Tilknyttet klasse")
-
-Role.create(name: "Student")
-Role.create(name: "Teacher")
-Role.create(name: "Mentor")
-Role.create(name: "Admin")
-Role.create(name: "Super admin")
-
-User.create(username: "kaspervh", email:"kaspervhauschildt@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: 5, grade_id: 1)
-User.create(username: "kalle", email:"kalle@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: 4, grade_id: 1)
-User.create(username: "kim", email: "kim@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: 2, grade_id: 1)
-User.create(username: "christian", email: "christian@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: 3, grade_id: 1)
-User.create(username: "keneth", email: "keneth@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: 1, grade_id: 1)
+User.create(username: "super admin", email:"kaspervhauschildt@gmail.com", password_digest: "tvebakk1", school_id: super_admin, role_id: 5, grade_id: 1)
+User.create(username: "admin", email:"kalle@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: admin, grade_id: 1)
+User.create(username: "lære", email: "kim@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: teacher, grade_id: 1)
+User.create(username: "special pedagog", email: "christian@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: mentor, grade_id: 1)
+User.create(username: "elev", email: "keneth@gmail.com", password_digest: "tvebakk1", school_id: 1, role_id: student, grade_id: 1)
