4ff9c01c6b81b688286965afd3f5ce1bea7d3a74
kaspervh <kaspervhauschildt@gmail.com>
2017-02-20 14:37:37 +0100

    fixed the scheduel sizing

--- app/models/scheduel.rb
+++ app/models/scheduel.rb
@@ -2,7 +2,7 @@ class Scheduel < ApplicationRecord
   belongs_to :grade
   
    has_attached_file :image,
-                      styles:{slide: "1000x500#", thumb: "65x65#"}
+                      styles:{slide: "3500x300", thumb: "65x65#"}
 
     validates_attachment :image,
                           content_type: { content_type: ["image/jpeg", "image/png"] }
